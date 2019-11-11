fdd217b6c4bcb166e3df262c89440b985136e726
Fatih Ersoy <f.ersoytx@gmail.com>
2019-11-10 23:17:52 -0600

    basic schema and routes has been created

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require("express");
+app = express();
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/BlogApp", { useNewUrlParser: true, useUnifiedTopology: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test blog",
+//     image: "https://s3.amazonaws.com/uploads.hotmart.com/blog/2017/09/criar-um-blog-2-670x419.png",
+//     body: " This is a Test blog!"
+// });
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("app starts here!")
+});
